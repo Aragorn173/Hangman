cbf006c6a98493b9b865f578abaf9fa03737f6df
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-02-01 14:42:34 +0100

    skapade en lista för understräcken så man kan gämföra varje plats. Sedan skrev jag om och använde en while loop för att veta om spelaren vinner eller förlorar.

--- src/hangman.java
+++ src/hangman.java
@@ -1,26 +1,58 @@
 import javax.swing.*;
+import java.sql.SQLOutput;
 
 public class hangman {
     public static void main(String[] args) {
         String word = JOptionPane.showInputDialog("Word?");
         String underscore = "";
+        String guess = "";
         char letterguess = 0;
         String wordguess = "";
+        int lives = 3;
+        int correct = 0;
         char[] letters = new char[word.length()];
 
-        for(int i = 0;i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             letters[i] = word.charAt(i);
             if (letters[i] == ' ') {
                 underscore += " ";
 
             } else if (letters[i] == '-') {
-                underscore += " -";
+                underscore += "-";
 
             } else {
-                underscore += " _";
+                underscore += "_";
             }
         }
-        System.out.println(underscore);
+        char[] underscorelist = new char[underscore.length()];
+        for (int e = 0; e < underscore.length(); e++) {
+            underscorelist[e] = underscore.charAt(e);
+        }
+        while (lives > 0) {
+            System.out.println(underscorelist);
+            guess = JOptionPane.showInputDialog("Guess?");
+            if (guess.length() > 1) {
+                wordguess = guess;
+                if (wordguess.equals(word)) {
+                    System.out.println("Conrgrats you win!\n" + "The word was " + word);
+                    System.exit(0);
+                } else {
+                    System.out.println("You lose!\n" + "The word was " + word + "\nYou guessed" + wordguess);
+                    System.exit(0);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        /*
 
 
         for (int g = 0; g < 1000; g++) {
@@ -49,7 +81,7 @@ public class hangman {
 
             } else {
 
-            }
+            }*/
         }
     }
 }
